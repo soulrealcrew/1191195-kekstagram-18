57269cda9cfb9a07c4a0abb27993057b7359a765
Keks <keks@htmlacademy.ru>
2019-10-19 11:01:07 +0300

    :heavy_check_mark: Сборка #10

--- 10/js/backend.js
+++ 10/js/backend.js
@@ -9,7 +9,7 @@
     DATA: BASE_URL + '/data',
   };
 
-  var download = function (onSuccess, onError) {
+  var createXhr = function (onSuccess, onError) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
     xhr.timeout = LOAD_TIMEOUT;
@@ -30,33 +30,19 @@
       onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
     });
 
-    xhr.open('GET', urls.DATA);
-    xhr.send();
+    return xhr;
   };
 
   var upload = function (data, onSuccess, onError) {
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
-    xhr.timeout = LOAD_TIMEOUT;
-
-    xhr.addEventListener('load', function () {
-      if (xhr.status === SUCCESS_STATUS) {
-        onSuccess(xhr.response);
-      } else {
-        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-      }
-    });
-
-    xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения');
-    });
-
-    xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-    });
-
+    var xhr = createXhr(onSuccess, onError);
     xhr.open('POST', BASE_URL);
     xhr.send(data);
+  };
+
+  var download = function (onSuccess, onError) {
+    var xhr = createXhr(onSuccess, onError);
+    xhr.open('GET', urls.DATA);
+    xhr.send();
   };
 
   window.backend = {
